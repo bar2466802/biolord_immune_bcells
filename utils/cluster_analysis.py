4ac1b7888eac747c94049d109ff95b3934a234a3
bar246802 <bar246802@gmail.com>
2023-03-06 15:29:21 +0200

    Add rand_index func

--- utils/cluster_analysis.py
+++ utils/cluster_analysis.py
@@ -1,4 +1,27 @@
-import numpy as np
+################################################
+# FILE: cluster_analysis.py
+# WRITERS: Bar Melinarskiy
+# DESCRIPTION: Utils functions for evaluating the received latent space clusters
+################################################
 
-def k_means():
-    pass
+# Imports
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+from sklearn.metrics import rand_score
+
+
+# Functions
+def rand_index(df: pd.DataFrame, k: int, ground_truth_labels: np.ndarray):
+    """
+    Evaluate the latent space clustering, comparing known labels to kmeans clustering using the Rand index score
+    :param df: The latent space data
+    :param k: Number of clusters - usually the number of labels (cell types / organs / etc...)
+    :param ground_truth_labels: array of known labels for each sample
+    :return: rand index score
+    """
+    km = KMeans(n_clusters=k)
+    labels = km.fit_predict(df)
+    return rand_score(ground_truth_labels, labels)
