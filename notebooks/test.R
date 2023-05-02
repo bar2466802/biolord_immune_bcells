41d6236fa9db3bb40e6fdc9d17661e461eaaa83b
Bar Melinarskiy <bar246802@gmail.com>
2023-05-02 19:37:12 +0300

    test.R

--- notebooks/test.R
+++ notebooks/test.R
@@ -1,12 +1,11 @@
-remotes::install_github("YuLab-SMU/clusterProfiler", force = TRUE) 
+# remotes::install_github("YuLab-SMU/clusterProfiler", force = TRUE, lib="4.3") 
 # install the packages
-remotes::install_github("YuLab-SMU/createKEGGdb", force = TRUE)
+# remotes::install_github("YuLab-SMU/createKEGGdb", force = TRUE, lib="4.3")
 # import the library and create a KEGG database locally 
-library(createKEGGdb)
-species <-c("ath","hsa","mmu", "rno","dre","dme","cel")
-createKEGGdb::create_kegg_db(species)
+# library(createKEGGdb)
+# species <-c("ath","hsa","mmu", "rno","dre","dme","cel")
+# createKEGGdb::create_kegg_db(species)
 # You will get KEGG.db_1.0.tar.gz file in your working directory
-
 
 library(clusterProfiler)
 library(org.Hs.eg.db)
@@ -23,3 +22,4 @@ run1 <- gseKEGG(geneList     = geneList,
                                 pvalueCutoff  = 1,
                                 verbose       = FALSE,
                                 seed          = TRUE)
+run1
